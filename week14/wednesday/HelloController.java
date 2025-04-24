ddc83e130efbe9912b7e581892c5dc07ca40d81d
Jin An <jin.an@dtcc.edu>
2025-04-24 12:02:44 -0400

    Update HelloController.java

--- week14/wednesday/HelloController.java
+++ week14/wednesday/HelloController.java
@@ -1,23 +1,112 @@
 package com.example.week14listview;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+public class HelloController {
+    private static ArrayList<Bowler> bowlers = new ArrayList<>();
+
+    @FXML
+    private ListView<Bowler> bowlerListView;
+
+    @FXML
+    private Label welcomeText;
+
+    private void loadBowlersIntoListView() {
+        for (Bowler each: bowlers) {
+            bowlerListView.getItems().add(each);
+        }
     }
 
-    public static void main(String[] args) {
-        launch();
+    @FXML
+    void showGoodBowlers(ActionEvent event) {
+        bowlerListView.getItems().clear();
+        for (Bowler each: bowlers) {
+            if (each.getScore() >= 200)
+                bowlerListView.getItems().add(each);
+        }
+        bowlerListView.refresh();
+    }
+
+    public void initialize() {
+//        bowlers.add(new Bowler("Bryce Harper", 250));
+//        bowlers.add(new Bowler("Zach Wheeler", 290));
+//        bowlers.add(new Bowler("Joel Embiid", 110));
+//        bowlers.add(new Bowler("Ben Simmons", 49));
+
+        try {
+            Scanner input = new Scanner(new File("bowlers.csv"));
+
+            // throw away header row
+            input.nextLine();
+
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                String[] lineParts = line.split(",");
+
+                bowlers.add(new Bowler(lineParts[0].trim(), Integer.parseInt(lineParts[1].trim())));
+
+            }
+        } catch (FileNotFoundException error) {
+            // not good
+        }
+
+
+        try {
+            Scanner fileReader = new Scanner(new File("bowlerData.txt"));
+            while (fileReader.hasNextLine()) {
+                String bowlerName = fileReader.nextLine();
+                String bowlerScore = fileReader.nextLine();
+
+                // bowlers.add(new Bowler(bowlerName, Integer.parseInt(bowlerScore)));
+
+
+                //String bowlerName = fileReader.nextLine();
+                //int bowlerScore = fileReader.nextInt();
+                //Bowler bowler = new Bowler(bowlerName, bowlerScore);
+                //bowlers.add(bowler);
+
+
+                //could do it directly
+//                bowlers.add(new Bowler(fileReader.nextLine(), fileReader.nextInt()));
+//                fileReader.nextLine(); // this is a hack..
+
+
+            }
+        } catch (FileNotFoundException error) {
+            System.out.println("File was not found!");
+        }
+
+        loadBowlersIntoListView();
+    }
+
+}
+
+class Bowler {
+    private String name;
+    private int score;
+
+    public Bowler(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "Bowler{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
     }
 }
